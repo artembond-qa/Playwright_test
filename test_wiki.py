a39d81d6fbf66b01a74d098a2eee4bea62ad11f7
Artem Bondarenko <artembondarenko7780@gmail.com>
2026-09-21 22:28:02 +0300

    yml file

--- test_wiki.py
+++ test_wiki.py
@@ -4,8 +4,21 @@ from playwright.sync_api import Page, expect
 def test_wiki(page: Page):
     page.goto("https://ru.wikipedia.org/")
 
-    page.get_by_role("link",name="Оборотень").first.click()
+    page.get_by_role("link",name="Нацистская Германия").first.click()
 
-    heading = page.get_by_role("heading", name="Сюжет")
+    heading = page.get_by_role("heading", name="Название государства")
 
     expect(heading).to_be_visible()
+
+def test_add_todo(page: Page):
+    page.goto("https://demo.playwright.dev/todomvc/")
+
+    todo_input = page.get_by_placeholder("What needs to be done?")
+
+    todo_input.fill("Learn Playwright")
+    todo_input.press("Enter")
+
+    todo_item = page.get_by_test_id("todo-item")
+
+    expect(todo_item).to_have_count(1)
+    expect(todo_item).to_contain_text("Learn Playwright")
